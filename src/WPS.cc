9403b9aee41516e2b06370aaafb40f089bbb8439
James Raspass <jraspass@gmail.com>
2012-10-12 06:05:51 +0100

    cleanup

--- src/WPS.cc
+++ src/WPS.cc
@@ -6,69 +6,69 @@
 #include <stdio.h>
 #include <string>
 
-class Interface : public WPXDocumentInterface
+class Interface :public WPXDocumentInterface
 {
 	private:
 		unsigned short i;
 
 	public:
-		         Interface(){}
-		virtual ~Interface(){}
-		virtual void closeComment(){}
-		virtual void closeEndnote(){}
-		virtual void closeFooter(){}
-		virtual void closeFootnote(){}
-		virtual void closeFrame(){}
-		virtual void closeHeader(){}
-		virtual void closeOrderedListLevel(){}
-		virtual void closePageSpan(){}
-		virtual void closeSection(){}
-		virtual void closeSpan(){}
-		virtual void closeTable(){}
-		virtual void closeTableCell(){}
-		virtual void closeTableRow(){}
-		virtual void closeTextBox(){}
-		virtual void closeUnorderedListLevel(){}
-		virtual void defineCharacterStyle( const WPXPropertyList& ){}
-		virtual void defineOrderedListLevel( const WPXPropertyList& ){}
-		virtual void definePageStyle( const WPXPropertyList& ){}
-		virtual void defineParagraphStyle( const WPXPropertyList&, const WPXPropertyListVector& ){}
-		virtual void defineSectionStyle( const WPXPropertyList&, const WPXPropertyListVector& ){}
-		virtual void defineUnorderedListLevel( const WPXPropertyList& ){}
-		virtual void endDocument(){}
-		virtual void insertBinaryObject( const WPXPropertyList&, const WPXBinaryData& ){}
-		virtual void insertCoveredTableCell( const WPXPropertyList& ){}
-		virtual void insertEquation( const WPXPropertyList&, const WPXString& ){}
-		virtual void insertField( const WPXString&, const WPXPropertyList& ){}
-		virtual void openComment( const WPXPropertyList& ){}
-		virtual void openEndnote( const WPXPropertyList& ){}
-		virtual void openFooter( const WPXPropertyList& ){}
-		virtual void openFootnote( const WPXPropertyList& ){}
-		virtual void openFrame( const WPXPropertyList& ){}
-		virtual void openHeader( const WPXPropertyList& ){}
-		virtual void openPageSpan( const WPXPropertyList& ){}
-		virtual void openParagraph( const WPXPropertyList&, const WPXPropertyListVector& ){}
-		virtual void openSection( const WPXPropertyList&, const WPXPropertyListVector& ){}
-		virtual void openSpan( const WPXPropertyList& ){}
-		virtual void openTable( const WPXPropertyList&, const WPXPropertyListVector& ){}
-		virtual void openTableCell( const WPXPropertyList& ){}
-		virtual void openTableRow( const WPXPropertyList& ){}
-		virtual void openTextBox( const WPXPropertyList& ){}
-		virtual void openUnorderedListLevel( const WPXPropertyList& ){}
-		virtual void setDocumentMetaData( const WPXPropertyList& ){}
-		virtual void startDocument(){}
+		     Interface(){}
+		    ~Interface(){}
+		void closeComment(){}
+		void closeEndnote(){}
+		void closeFooter(){}
+		void closeFootnote(){}
+		void closeFrame(){}
+		void closeHeader(){}
+		void closeOrderedListLevel(){}
+		void closePageSpan(){}
+		void closeSection(){}
+		void closeSpan(){}
+		void closeTable(){}
+		void closeTableCell(){}
+		void closeTableRow(){}
+		void closeTextBox(){}
+		void closeUnorderedListLevel(){}
+		void defineCharacterStyle( const WPXPropertyList& ){}
+		void defineOrderedListLevel( const WPXPropertyList& ){}
+		void definePageStyle( const WPXPropertyList& ){}
+		void defineParagraphStyle( const WPXPropertyList&, const WPXPropertyListVector& ){}
+		void defineSectionStyle( const WPXPropertyList&, const WPXPropertyListVector& ){}
+		void defineUnorderedListLevel( const WPXPropertyList& ){}
+		void endDocument(){}
+		void insertBinaryObject( const WPXPropertyList&, const WPXBinaryData& ){}
+		void insertCoveredTableCell( const WPXPropertyList& ){}
+		void insertEquation( const WPXPropertyList&, const WPXString& ){}
+		void insertField( const WPXString&, const WPXPropertyList& ){}
+		void openComment( const WPXPropertyList& ){}
+		void openEndnote( const WPXPropertyList& ){}
+		void openFooter( const WPXPropertyList& ){}
+		void openFootnote( const WPXPropertyList& ){}
+		void openFrame( const WPXPropertyList& ){}
+		void openHeader( const WPXPropertyList& ){}
+		void openPageSpan( const WPXPropertyList& ){}
+		void openParagraph( const WPXPropertyList&, const WPXPropertyListVector& ){}
+		void openSection( const WPXPropertyList&, const WPXPropertyListVector& ){}
+		void openSpan( const WPXPropertyList& ){}
+		void openTable( const WPXPropertyList&, const WPXPropertyListVector& ){}
+		void openTableCell( const WPXPropertyList& ){}
+		void openTableRow( const WPXPropertyList& ){}
+		void openTextBox( const WPXPropertyList& ){}
+		void openUnorderedListLevel( const WPXPropertyList& ){}
+		void setDocumentMetaData( const WPXPropertyList& ){}
+		void startDocument(){}
 
 		std::stringstream ss;
 
-		virtual void closeListElement()                             { ss << "\n";        }
-		virtual void closeParagraph()                               { ss << "\n";        }
-		virtual void insertLineBreak()                              { ss << "\n";        }
-		virtual void insertSpace()                                  { ss <<  " ";        }
-		virtual void insertTab()                                    { ss << "\t";        }
-		virtual void insertText(      const WPXString& txt         ){ ss << txt.cstr();  }
-		virtual void openListElement( const WPXPropertyList&,
-		                              const WPXPropertyListVector& ){ ss << ++i << ". "; }
-		virtual void openOrderedListLevel( const WPXPropertyList&  ){ i = 0;             }
+		void closeListElement()                             { ss << "\n";        }
+		void closeParagraph()                               { ss << "\n";        }
+		void insertLineBreak()                              { ss << "\n";        }
+		void insertSpace()                                  { ss <<  " ";        }
+		void insertTab()                                    { ss << "\t";        }
+		void insertText(      const WPXString& txt         ){ ss << txt.cstr();  }
+		void openListElement( const WPXPropertyList&,
+		                      const WPXPropertyListVector& ){ ss << ++i << ". "; }
+		void openOrderedListLevel( const WPXPropertyList&  ){ i = 0;             }
 };
 
 std::string WPS::parse( const std::string& filename )
